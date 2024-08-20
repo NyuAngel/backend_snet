7235f94740b9aee2227e77c84c63f59b817145e9
Viviana Angel <lvfandino@misena.edu.co>
2024-08-20 12:44:51 -0500

    delete file start-project.bat

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connection = async()=> {
     try {
         await connect (process.env.MONGODB_URI);
-        console.log ("Conectado correctamente a db_social_net_DWFSV3-179 ");
+        console.log ("Conectado correctamente a la base de de datos de Atlas db_social_net_DWFSV3-179 ");
     }   catch (error) {
         console.log ("Error al conectar la DB" , error);
         throw new Error ( "¡No se ha podido conectar a la base de datos!");
--- start-project.bat
+++
@@ -1,3 +0,0 @@
-
-
-start cmd.exe /k "cd / C:\Bootcamp_Talento_Tech\final_project\backend_snet && npm run start:watch"
