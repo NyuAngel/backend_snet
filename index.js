635038738f3c15d6967679309a5ac50a09ea09be
NyuAngel <lvfandino@misena.edu.co>
2024-09-18 07:16:58 -0500

    upload

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ import cors from "cors";
 import UserRoutes from "./routes/user.js";
 import PublicationRoutes from "./routes/publication.js";
 import FollowRoutes from "./routes/follow.js";
-import path from 'path';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import dotenv from "dotenv";
+import { v2 as cloudinary } from 'cloudinary';
 
+dotenv.config();
+
+// Configurar Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 // Mensaje de bienvenida para verificar que ejecutó bien la API de Node
 console.log("API Node en ejecución");
@@ -37,19 +44,9 @@ app.use('/api/user', UserRoutes);
 app.use('/api/publication', PublicationRoutes);
 app.use('/api/follow', FollowRoutes);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Configuración para servir archivos estáticos (imágenes de avatar)
-app.use('/uploads/avatars', express.static(path.join(__dirname, 'uploads', 'avatars')));
-
-// Configuración para servir archivos estáticos (imágenes de publicaciones)
-app.use('/uploads/publications', express.static(path.join(__dirname, 'uploads', 'publications')));
-
 // Configurar el servidor Node
 app.listen(puerto, () => {
   console.log("Servidor de Node ejecutándose en el puerto", puerto);
 });
 
 export default app;
-
--- services/jwt.js
+++ services/jwt.js
@@ -2,7 +2,7 @@ import jwt from  'jwt-simple';
 import moment from 'moment';
 import dotenv from 'dotenv';
 
-//Cargar variables de entorno desde el archivo .env
+// Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
 // Clave secreta
@@ -12,11 +12,11 @@ const secret = process.env.SECRET_KEY;
 const createToken = (user) => {
   const payload = {
     userId: user._id,
-    name:user.name, // quitar para el despliegue
+    name: user.name,  // quitar para el despliegue
     role: user.role,
     // Fecha de emisión
     iat: moment().unix(),
-    exp: moment().add(30, 'days').unix()
+    exp: moment().add(7, 'days').unix()
   };
 
   // Devolver jwt_token codificado
